d30011b55fab9935108ac0983b358172af82dd1c
Patmorca <patmika1@gmail.com>
2024-08-24 15:12:30 +1000

    Tested passing things between classes. Works well.

--- FrameLoop.py
+++ FrameLoop.py
@@ -3,6 +3,7 @@
 ## Not too sure how python hands passing objects, whether they're copied and passed by value or passed by reference and can be edited from within the caller.
 ## Will need to investigate when things are up and running to see.
 
+from logging import root
 import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
@@ -14,29 +15,41 @@ import _tkinter as tk
 
 class GestureVision:
     
-    def __init__(self): ## Initilises all MP and CV variables and objects to be operated on
+    def __init__(self,root,window): ## Initilises all MP and CV variables and objects to be operated on
         self.frameCapture = cv2.VideoCapture(0,cv2.CAP_DSHOW)
         self.mpHands = mp.solutions.hands
         self.mpDrawing = mp.solutions.drawing_utils
         self.mpHandObject = self.mpHands.Hands()
         
-    
-    def updateFrame(self,MPObject): ## This might need a reference to the UI Root, not too sure - same problem as listed at top
+        ## UI REFERENCES ##
+       
+        self.root = root 
+        self.window = window
+
+        
+    ##Needs MPObject param
+    def updateFrame(self): ## This might need a reference to the UI Root, not too sure - same problem as listed at top
         success, frame = self.frameCapture.read()
         if success:
             frameRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) ## OpenCV takes images in BGR format, this converts them into the proper RGB format for display and processing
             results = self.mpHandObject.process(frameRGB)
         
-        gestureFrame = Image.fromarray(frameRGB)
+            gestureFrame = Image.fromarray(frameRGB)
 
-        MPObject.recognizeGesture(gestureFrame) ## MPObject will be a globally defined MPRecognizer object declared within the main ui loop
+        ## MPObject.recognizeGesture(gestureFrame) ## MPObject will be a globally defined MPRecognizer object declared within the main ui loop
+            
+            resizedFrame = gestureFrame.resize((320,240),Image.Resampling.LANCZOS)
+            displayFrame = ImageTk.PhotoImage(image = resizedFrame)
+       
+       ## return it to the tkinter widget in which we want to display it
+       
+            self.window.image = displayFrame
+            self.window.config(image=displayFrame)
+            self.root.after(20,self.updateFrame)
+        else:
+            return
         
-        displayFrame = ImageTk.PhotoImage(image = (frameRGB.resize((320,240),Image.Resampling.LANCZOS)))
-        ## return it to the tkinter widget in which we want to display it
-        
-        
-
-
+      
     def drawLandmarks(self): ## Draws hand landmarks. Good debugging tool but unnecessary to do all the time. Could add as boolean option
         return
     
--- Main.py
+++ Main.py
@@ -1,1 +1,41 @@
-## PUT UI AND CALL MODULE CALL LOGIC IN HERE ##
+## PUT UI AND MODULE CALL LOGIC IN HERE ##
+import MPRecognition
+import FrameLoop
+import Functions
+
+import tkinter as tk
+
+
+
+## Instead of using a different class to create the UI, it will be easier (both logistically and layout-wise) to just write the UI in here.
+## Get rid of all the testing stuff when you're ready to put the UI down. This was just a test to see if I could get things passing properly between class modules.
+
+
+## TEST UI ##
+
+root = tk.Tk()
+root.title("Gesture Based Image Manipulation")
+root.geometry("1280x720")
+
+deviceCamera = tk.Label(master=root)
+deviceCamera.place(relx=1.0,rely=1.0,x=0,y=0,anchor='se')
+
+## TEST UI
+
+
+## TEST MATERIAL ##
+
+looper = FrameLoop.GestureVision(root,deviceCamera) ##instantiates gesturevision object (frameloop), passes references to ui root and device camera widget
+
+def startCamera():
+    looper.updateFrame()
+    
+
+startButton = tk.Button(master=root,text="Start Camera",command = startCamera)
+startButton.place(relx=0.5,rely=0.9,anchor='center')
+
+## TEST MATERIAL ##
+
+if __name__ == "__main__":
+    root.mainloop()
+    
